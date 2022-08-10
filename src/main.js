96bdc96e300898ef618bde2f0852c7c2c25c3195
liuxu <a17505670612@163.com>
2022-08-10 14:46:00 +0800

    完成了登录功能

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+
+const app=createApp(App)
+//配置请求根路径
+axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
+app.config.globalProperties.$axios=axios
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+app.use(router).use(ElementPlus).mount('#app')
